00b9ad0f3524a86cab1fed35baf89b209e954404
Christopher <christopher.d.miller.1@gmail.com>
2014-07-13 14:42:03 -0400

    Changes to the Knighted project
    - Added move validation (isOpponent || !hasPiece)

--- Knighted/src/us/the/mac/knighted/pieces/Knight.java
+++ Knighted/src/us/the/mac/knighted/pieces/Knight.java
@@ -14,14 +14,29 @@ public class Knight extends Piece {
 
 	@Override
 	public void setUpMoves() {
-		move_1 = fromCurPos(MOVE_UP_LEFT + MOVE_LEFT);// 6
-		move_2 = fromCurPos(MOVE_UP_LEFT + MOVE_UP);// 15;
-		move_3 = fromCurPos(MOVE_UP_RIGHT + MOVE_UP);// 17;
-		move_4 = fromCurPos(MOVE_UP_RIGHT + MOVE_RIGHT);// 10;
-		move_5 = fromCurPos(MOVE_DOWN_RIGHT + MOVE_RIGHT);// -6;
-		move_6 = fromCurPos(MOVE_DOWN_RIGHT + MOVE_DOWN);// -15;
-		move_7 = fromCurPos(MOVE_DOWN_LEFT + MOVE_DOWN);// -17;
-		move_8 = fromCurPos(MOVE_DOWN_LEFT + MOVE_LEFT);// -10;
+		
+		move_1 = validateMove(MOVE_UP_LEFT + MOVE_LEFT);// 6
+		move_2 = validateMove(MOVE_UP_LEFT + MOVE_UP);// 15;
+		move_3 = validateMove(MOVE_UP_RIGHT + MOVE_UP);// 17;
+		move_4 = validateMove(MOVE_UP_RIGHT + MOVE_RIGHT);// 10;
+		move_5 = validateMove(MOVE_DOWN_RIGHT + MOVE_RIGHT);// -6;
+		move_6 = validateMove(MOVE_DOWN_RIGHT + MOVE_DOWN);// -15;
+		move_7 = validateMove(MOVE_DOWN_LEFT + MOVE_DOWN);// -17;
+		move_8 = validateMove(MOVE_DOWN_LEFT + MOVE_LEFT);// -10;
+	}
+	
+	private int validateMove(int move) {
+		
+		int possibleMove = fromCurPos(move);
+		Piece p = getPieceAt(possibleMove);
+		
+		boolean hasPiece = p != null;
+		boolean isOpponent = hasPiece && p.isOpponent(color);
+		
+		if(isOpponent || !hasPiece) 
+			return possibleMove;
+		
+		return INVALID_MOVE;
 	}
 
 	@Override
